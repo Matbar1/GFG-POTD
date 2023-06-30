a9a3d12ce9fe63e17893fa987514a465523dfd7c
Matbar1 <136784491+Matbar1@users.noreply.github.com>
2023-06-30 10:11:20 +0530

    Add files via upload

--- 06.cpp
+++ 06.cpp
@@ -1,24 +1,18 @@
 class Solution{
-  public:
-    /*You are required to complete this method*/
-    int maxDepth(Node *root) {
-        if (root == NULL) return 0;
-    
-        if (root->left == NULL and root->right == NULL) return 1;
-        // no child nodes indicate base case
-    
-        int left, right;
-        if (root->left != NULL) left = maxDepth(root->left);
-        // finding depth of left sub tree
-        else
-            left = INT_MIN;
-    
-        if (root->right != NULL) right = maxDepth(root->right);
-        // finding depth of right sub tree
-        else
-            right = INT_MIN;
-    
-        return 1 + max(left, right);
-    }
+public:	
+		
+int isDivisible(string s){
+	    int sum = 0, x = 1;
+	    for(int i = s.length() - 1; i >= 0; i--){
+	        if((s[i] - '0') == 1){
+	            sum = (sum + x) % 3;
+	        }
+	        x = (x * 2) % 3;
+	    }
+
+	    if(sum == 0) return 1;
+	    return 0;
+	}
+
 
 };
